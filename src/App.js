2d7bb00bfacb4521b7952c3686f9fb9de9181d31
Farrel ganteng <valensireptorz97@gmail.com>
2022-11-03 15:29:48 +0700

    firt commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="Parentbox">
+      <div className="Foto">
+        <img src="foto.jpg" />
+      </div>
+      <div className="Deskripsi">
+        <p className="Cate">  Air Force 1</p>
+        <h1 className="Title">Disc Up to 30%</h1>
+        <p className="Price">Rp. 1.500.000</p>
+        <p className="Info">
+        Air Force 1
+An icon on the streets, the Nike Air Force 1 is among the most popular footwear designs of all time. 
+The Air Force 1 Low, Air Force 1 Mid, and Air Force 1 High in all of its classic looks and latest collaborations are here.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h3 className="Type">Type</h3>
+        <div className="Tipe">
+          <button className="White of white">White of white</button>
+          <button className="Black and white">Black and white</button>
+          <button className="Off-white-white">Off-white-white</button>
+          <button className="jewel NYC cool grey">jewel NYC cool grey</button>
+        </div>
+        <h3 className="Size">Size</h3>
+        <div className="Ukuran">
+          <button className="39">39</button>
+          <button className="42">42</button>
+          <button className="43">43</button>
+          <button className="44">44</button>
+        </div>
+        <button className="Beli">Buy Now</button>
+        <button className="Wish">Add To Wishlist</button>
+      </div>
     </div>
   );
 }
